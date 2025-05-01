5450dd72c83c6c5305535474ca8207eee1145351
Jrowez <jonathanrowe1998@gmail.com>
2025-04-30 23:22:09 -0500

    updated some loot sack transmogs added new loot sack transmogs adjusted loot sack rarity rolls

--- src/main/java/xyz/iwolfking/woldsvaults/models/LootSacks.java
+++ src/main/java/xyz/iwolfking/woldsvaults/models/LootSacks.java
@@ -10,24 +10,53 @@ public class LootSacks {
 
     public static final HandHeldModel BUNDLE;
     public static final HandHeldModel GRAY;
-    public static final HandHeldModel GREEN;
+
     public static final HandHeldModel LIGHT_BLUE;
-    public static final HandHeldModel PINK;
-    public static final HandHeldModel PURPLE;
     public static final HandHeldModel RED;
+
+    public static final HandHeldModel PINK;
     public static final HandHeldModel YELLOW;
+
+    public static final HandHeldModel PURPLE;
+
+    public static final HandHeldModel GREEN;
+    public static final HandHeldModel MIDNIGHT;
+    public static final HandHeldModel PRISMATIC;
+
+    //unique
+    public static final HandHeldModel TINKERS_TANKARD;
+
 
     public LootSacks() {
     }
 
     static {
+        //scrappy
         BUNDLE = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/bundle"), "Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
         GRAY = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/gray"), "Gray Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
-        GREEN = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/green"), "Green Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
+
+        //common
         LIGHT_BLUE = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/light_blue"), "Light Blue Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
-        PINK = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/pink"), "Pink Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
-        PURPLE = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/purple"), "Purple Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
         RED = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/red"), "Red Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
+
+        //rare
+        PINK = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/pink"), "Pink Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
         YELLOW = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/yellow"), "Yellow Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
+
+        //epic
+        PURPLE = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/purple"), "Purple Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
+        GREEN = (HandHeldModel)REGISTRY.register((HandHeldModel)(new HandHeldModel(VaultMod.id("gear/loot_sack/green"), "Green Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll()));
+
+        //omega
+        MIDNIGHT = REGISTRY.register(new HandHeldModel(VaultMod.id("gear/loot_sack/midnight"), "Midnight Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+        PRISMATIC = REGISTRY.register(new HandHeldModel(VaultMod.id("gear/loot_sack/prismatic"), "Prismatic Bundle Sack")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+        TINKERS_TANKARD = REGISTRY.register(new HandHeldModel(VaultMod.id("gear/loot_sack/tinkers_tankard"), "Loot Tankard")).properties(new DynamicModelProperties().allowTransmogrification().discoverOnRoll());
+
+        //unique
+
+        //ascension forge
+
+        //unused
+
     }
 }
