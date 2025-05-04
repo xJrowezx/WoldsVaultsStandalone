2163fa987b31f4bf93f6608df439d4b029ddba8f
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2025-05-04 00:11:25 -0500

    USE THE VAULT TRIDENTS MODEL

--- src/main/java/xyz/iwolfking/woldsvaults/mixin/MixinThrownTridentRenderer.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixin/MixinThrownTridentRenderer.java
@@ -1,40 +1,42 @@
 package xyz.iwolfking.woldsvaults.mixin;
 
-import iskallia.vault.VaultMod;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.ThrownTridentRenderer;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.projectile.ThrownTrident;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import xyz.iwolfking.vhapi.api.util.ResourceLocUtils;
-import xyz.iwolfking.vhapi.api.util.vhapi.VHAPILoggerUtils;
-import xyz.iwolfking.woldsvaults.WoldsVaults;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.iwolfking.woldsvaults.items.gear.VaultTridentItem;
+
+import javax.annotation.Nonnull;
 
 @Mixin(ThrownTridentRenderer.class)
 public abstract class MixinThrownTridentRenderer extends EntityRenderer<ThrownTrident> {
-
-    private static final ResourceLocation trident0 = VaultMod.id("gear/trident/trident_0");
-
-
     protected MixinThrownTridentRenderer(EntityRendererProvider.Context p_174008_) {
         super(p_174008_);
     }
 
-    @Inject(at = @At("HEAD"), method = "getTextureLocation(Lnet/minecraft/world/entity/projectile/ThrownTrident;)Lnet/minecraft/resources/ResourceLocation;", cancellable = true)
-    private void getTextureLocation(ThrownTrident trident, CallbackInfoReturnable<ResourceLocation> cir) {
-        if(((ThrownTridentAccessor)trident).callGetPickupItem().getItem()  instanceof VaultTridentItem item)  {
-            ItemStack tridentStack = ((ThrownTridentAccessor) trident).callGetPickupItem();
-            if(item.getDynamicModelId(tridentStack).isPresent()) {
-                cir.setReturnValue(ResourceLocation.tryParse(item.getDynamicModelId(tridentStack).get().toString() + ".png"));
-            }
+    @Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/world/entity/projectile/ThrownTrident;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", cancellable = true)
+    private void render(ThrownTrident trident, float yaw, float partialTicks, PoseStack matrix, @Nonnull MultiBufferSource buffer, int light, CallbackInfo ci) {
+        ItemStack itemStack = ((ThrownTridentAccessor) trident).getTridentItem();
+        if (itemStack.getItem() instanceof VaultTridentItem)  {
+            matrix.pushPose();
+            matrix.mulPose(Vector3f.YP.rotationDegrees(Mth.lerp(partialTicks, trident.yRotO, trident.getYRot()) - 90.0F));
+            matrix.mulPose(Vector3f.ZP.rotationDegrees(Mth.lerp(partialTicks, trident.xRotO, trident.getXRot())));
+            Minecraft.getInstance().getItemRenderer().renderStatic(itemStack, ItemTransforms.TransformType.FIXED, light, OverlayTexture.NO_OVERLAY, matrix, buffer, 0);
+            matrix.popPose();
+            super.render(trident, yaw, partialTicks, matrix, buffer, light);
+            ci.cancel();
         }
     }
-
-
 }
--- src/main/java/xyz/iwolfking/woldsvaults/mixin/ThrownTridentAccessor.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixin/ThrownTridentAccessor.java
@@ -3,12 +3,10 @@ package xyz.iwolfking.woldsvaults.mixin;
 import net.minecraft.world.entity.projectile.ThrownTrident;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ThrownTrident.class)
 public interface ThrownTridentAccessor {
-    @Invoker
-    ItemStack callGetPickupItem();
-
-
+    @Accessor ItemStack getTridentItem();
 }
