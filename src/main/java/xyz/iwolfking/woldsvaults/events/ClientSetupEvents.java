ee80326799b60b4063735b8e0315d7d984d70578
JustAHuman-xD <imjustahuman.xd@gmail.com>
2025-07-30 12:52:32 -0500

    move to dedicated client class

--- src/main/java/xyz/iwolfking/woldsvaults/events/ClientSetupEvents.java
+++ src/main/java/xyz/iwolfking/woldsvaults/events/ClientSetupEvents.java
@@ -1,12 +1,17 @@
 package xyz.iwolfking.woldsvaults.events;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.ParticleEngine;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import xyz.iwolfking.woldsvaults.init.ModParticles;
 import xyz.iwolfking.woldsvaults.init.client.ModEntityRenderers;
 import xyz.iwolfking.woldsvaults.init.client.ModScreens;
+import xyz.iwolfking.woldsvaults.particle.BloodParticle;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = {Dist.CLIENT}, modid = "woldsvaultsstandalone")
 public class ClientSetupEvents {
@@ -14,5 +19,11 @@ public class ClientSetupEvents {
     public static void setupClient(FMLClientSetupEvent event) {
         ModScreens.register();
         ModEntityRenderers.register(event);
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void registerParticles(ParticleFactoryRegisterEvent event) {
+        ParticleEngine particleManager = Minecraft.getInstance().particleEngine;
+        particleManager.register(ModParticles.BLOOD_PARTICLE, BloodParticle.Factory::new);
     }
 }
--- src/main/java/xyz/iwolfking/woldsvaults/init/ModParticles.java
+++ src/main/java/xyz/iwolfking/woldsvaults/init/ModParticles.java
@@ -1,24 +1,10 @@
 package xyz.iwolfking.woldsvaults.init;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.particle.ParticleEngine;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import xyz.iwolfking.woldsvaults.WoldsVaults;
-import xyz.iwolfking.woldsvaults.particle.BloodParticle;
 
-@Mod.EventBusSubscriber(
-        modid = WoldsVaults.MODID,
-        bus = Mod.EventBusSubscriber.Bus.MOD,
-        value = Dist.CLIENT
-)
 public class ModParticles {
     public static final SimpleParticleType BLOOD_PARTICLE = simple("blood", true);
 
@@ -30,12 +16,5 @@ public class ModParticles {
 
     public static void register(RegistryEvent.Register<ParticleType<?>> event) {
         event.getRegistry().registerAll(BLOOD_PARTICLE);
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void registerParticles(ParticleFactoryRegisterEvent event) {
-        ParticleEngine particleManager = Minecraft.getInstance().particleEngine;
-        particleManager.register(BLOOD_PARTICLE, BloodParticle.Factory::new);
     }
 }
