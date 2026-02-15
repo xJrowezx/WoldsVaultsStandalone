5d630eb767913ef1ffcb69694942f2b20e587759
MrBustin <redlegoboy1@gmail.com>
2026-02-15 18:43:19 -0500

    Fixed Server sided crash from BEEG backpack caused by RegistryEvents

--- src/main/java/xyz/iwolfking/woldsvaults/events/RegistryEvents.java
+++ src/main/java/xyz/iwolfking/woldsvaults/events/RegistryEvents.java
@@ -88,38 +88,4 @@ public class RegistryEvents {
         ModGearModifications.init(event);
     }
 
-
-
-    @SubscribeEvent
-    public static void registerBlockColorHandles(ColorHandlerEvent.Block event) {
-        event.getBlockColors().register((state, blockDisplayReader, pos, tintIndex) -> {
-            if(tintIndex < 0 || tintIndex > 1 || pos == null) {
-                return -1;
-            }
-            return WorldHelper.getBlockEntity(blockDisplayReader, pos, BackpackBlockEntity.class).map(te -> tintIndex == 0 ? te.getBackpackWrapper().getMainColor() : te.getBackpackWrapper().getAccentColor()).orElse(getDefaultColor(tintIndex));
-        }, ModBlocks.XL_BACKPACK);
-    }
-
-    @SubscribeEvent
-    public static void registerItemColorHandlers(ColorHandlerEvent.Item event) {
-        event.getItemColors().register((backpack, layer) -> {
-            if(layer > 1 || !(backpack.getItem() instanceof BackpackItem)) {
-                return -1;
-            }
-            return backpack.getCapability(CapabilityBackpackWrapper.getCapabilityInstance()).map(backpackWrapper -> {
-                if(layer == 0) {
-                    return backpackWrapper.getMainColor();
-                }
-                else if(layer == 1) {
-                    return backpackWrapper.getAccentColor();
-                }
-                return -1;
-            }).orElse(BackpackWrapper.DEFAULT_CLOTH_COLOR);
-        }, ModItems.XL_BACKPACK);
-    }
-
-    public static int getDefaultColor(int tintIndex) {
-        return tintIndex == 0 ? BackpackWrapper.DEFAULT_CLOTH_COLOR : BackpackWrapper.DEFAULT_BORDER_COLOR;
-    }
-
 }
--- src/main/java/xyz/iwolfking/woldsvaults/init/client/ModModels.java
+++ src/main/java/xyz/iwolfking/woldsvaults/init/client/ModModels.java
@@ -4,8 +4,16 @@ import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.p3pp3rf1y.sophisticatedbackpacks.api.CapabilityBackpackWrapper;
+import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackBlockEntity;
+import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackItem;
+import net.p3pp3rf1y.sophisticatedbackpacks.backpack.wrapper.BackpackWrapper;
+import net.p3pp3rf1y.sophisticatedcore.util.WorldHelper;
 import xyz.iwolfking.woldsvaults.init.ModBlocks;
+import xyz.iwolfking.woldsvaults.init.ModItems;
 
 @Mod.EventBusSubscriber(value = {Dist.CLIENT}, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModModels {
@@ -15,35 +23,35 @@ public class ModModels {
     }
 
 
-//    @SubscribeEvent
-//    public static void registerBlockColorHandles(ColorHandlerEvent.Block event) {
-//        event.getBlockColors().register((state, blockDisplayReader, pos, tintIndex) -> {
-//            if(tintIndex < 0 || tintIndex > 1 || pos == null) {
-//                return -1;
-//            }
-//            return WorldHelper.getBlockEntity(blockDisplayReader, pos, BackpackBlockEntity.class).map(te -> tintIndex == 0 ? te.getBackpackWrapper().getMainColor() : te.getBackpackWrapper().getAccentColor()).orElse(getDefaultColor(tintIndex));
-//        }, ModBlocks.XL_BACKPACK);
-//    }
-//
-//    @SubscribeEvent
-//    public static void registerItemColorHandlers(ColorHandlerEvent.Item event) {
-//        event.getItemColors().register((backpack, layer) -> {
-//            if(layer > 1 || !(backpack.getItem() instanceof BackpackItem)) {
-//                return -1;
-//            }
-//            return backpack.getCapability(CapabilityBackpackWrapper.getCapabilityInstance()).map(backpackWrapper -> {
-//                if(layer == 0) {
-//                    return backpackWrapper.getMainColor();
-//                }
-//                else if(layer == 1) {
-//                    return backpackWrapper.getAccentColor();
-//                }
-//                return -1;
-//            }).orElse(BackpackWrapper.DEFAULT_CLOTH_COLOR);
-//        }, ModItems.XL_BACKPACK);
-//    }
-//
-//    public static int getDefaultColor(int tintIndex) {
-//        return tintIndex == 0 ? BackpackWrapper.DEFAULT_CLOTH_COLOR : BackpackWrapper.DEFAULT_BORDER_COLOR;
-//    }
+    @SubscribeEvent
+    public static void registerBlockColorHandles(ColorHandlerEvent.Block event) {
+        event.getBlockColors().register((state, blockDisplayReader, pos, tintIndex) -> {
+            if(tintIndex < 0 || tintIndex > 1 || pos == null) {
+                return -1;
+            }
+            return WorldHelper.getBlockEntity(blockDisplayReader, pos, BackpackBlockEntity.class).map(te -> tintIndex == 0 ? te.getBackpackWrapper().getMainColor() : te.getBackpackWrapper().getAccentColor()).orElse(getDefaultColor(tintIndex));
+        }, ModBlocks.XL_BACKPACK);
+    }
+
+    @SubscribeEvent
+    public static void registerItemColorHandlers(ColorHandlerEvent.Item event) {
+        event.getItemColors().register((backpack, layer) -> {
+            if(layer > 1 || !(backpack.getItem() instanceof BackpackItem)) {
+                return -1;
+            }
+            return backpack.getCapability(CapabilityBackpackWrapper.getCapabilityInstance()).map(backpackWrapper -> {
+                if(layer == 0) {
+                    return backpackWrapper.getMainColor();
+                }
+                else if(layer == 1) {
+                    return backpackWrapper.getAccentColor();
+                }
+                return -1;
+            }).orElse(BackpackWrapper.DEFAULT_CLOTH_COLOR);
+        }, ModItems.XL_BACKPACK);
+    }
+
+    public static int getDefaultColor(int tintIndex) {
+        return tintIndex == 0 ? BackpackWrapper.DEFAULT_CLOTH_COLOR : BackpackWrapper.DEFAULT_BORDER_COLOR;
+    }
 }
