0b47b1b41b933a1ee3422045e3d5c5249573cf82
attack8 <attackeight@gmail.com>
2026-02-22 15:14:51 -0500

    Fix Sticky

--- src/main/java/xyz/iwolfking/woldsvaults/mixin/the_vault/trinketer/MixinWallClimbingTrinket.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixin/the_vault/trinketer/MixinWallClimbingTrinket.java
@@ -1,8 +1,10 @@
 package xyz.iwolfking.woldsvaults.mixin.the_vault.trinketer;
 
+import iskallia.vault.client.data.ClientExpertiseData;
 import iskallia.vault.gear.trinket.effects.WallClimbingTrinket;
 import iskallia.vault.skill.base.Skill;
 import iskallia.vault.skill.expertise.type.TrinketerExpertise;
+import iskallia.vault.skill.tree.ExpertiseTree;
 import iskallia.vault.world.data.PlayerExpertisesData;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
@@ -15,10 +17,13 @@ public class MixinWallClimbingTrinket {
 
     @Redirect(method = "onTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;getSpeed()F"))
     private static float increaseWallClimbingSpeed(Player instance) {
-        if (!(instance instanceof ServerPlayer player)) {
-            return instance.getSpeed();
+        double enhancement;
+        if (instance instanceof ServerPlayer player) {
+            enhancement = PlayerExpertisesData.get(player.getLevel()).getExpertises(player).getAll(TrinketerExpertise.class, Skill::isUnlocked).stream().mapToDouble(TrinketerExpertise::getDamageAvoidanceChance).sum();
+        } else {
+            ExpertiseTree tree = ((AccessorClientExpertiseData) new ClientExpertiseData()).getExpertises();
+            enhancement = tree.getAll(TrinketerExpertise.class, Skill::isUnlocked).stream().mapToDouble(TrinketerExpertise::getDamageAvoidanceChance).sum();
         }
-        double enhancement = PlayerExpertisesData.get(player.getLevel()).getExpertises(player).getAll(TrinketerExpertise.class, Skill::isUnlocked).stream().mapToDouble(TrinketerExpertise::getDamageAvoidanceChance).sum();
 
         return (float) (instance.getSpeed() * (1 + enhancement));
     }
