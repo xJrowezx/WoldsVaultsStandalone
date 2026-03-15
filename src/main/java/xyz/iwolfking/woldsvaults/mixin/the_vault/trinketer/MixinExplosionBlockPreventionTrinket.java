afc0e60f9d05f859ebfebcc43aca10458d872f01
attack8 <attackeight@gmail.com>
2026-03-15 13:27:21 -0400

    More trinket reworks

--- src/main/java/xyz/iwolfking/woldsvaults/mixin/the_vault/trinketer/MixinExplosionBlockPreventionTrinket.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixin/the_vault/trinketer/MixinExplosionBlockPreventionTrinket.java
@@ -2,22 +2,22 @@ package xyz.iwolfking.woldsvaults.mixin.the_vault.trinketer;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import iskallia.vault.client.data.ClientExpertiseData;
-import iskallia.vault.gear.trinket.effects.DiceTrinket;
+import iskallia.vault.gear.trinket.effects.ExplosionBlockPreventionTrinket;
 import iskallia.vault.skill.base.Skill;
 import iskallia.vault.skill.expertise.type.TrinketerExpertise;
 import iskallia.vault.skill.tree.ExpertiseTree;
 import iskallia.vault.world.data.PlayerExpertisesData;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = DiceTrinket.class, remap = false)
-public class MixinDiceTrinket {
+@Mixin(value = ExplosionBlockPreventionTrinket.class, remap = false)
+public class MixinExplosionBlockPreventionTrinket {
 
-    @Redirect(method = "onLivingHurt", at = @At(value = "INVOKE", target = "Liskallia/vault/gear/trinket/effects/DiceTrinket$DiceConfig;getMinimumMultiplier()F"))
-    private static float increaseMinimumMultiplier(DiceTrinket.DiceConfig instance, @Local(argsOnly = true) LivingHurtEvent event) {
+    @Redirect(method = "onLivingHurt", at = @At(value = "INVOKE", target = "Liskallia/vault/gear/trinket/effects/ExplosionBlockPreventionTrinket$Config;getChance()F"))
+    private static float enhancePreventionChance(ExplosionBlockPreventionTrinket.Config instance, @Local(argsOnly = true) LivingAttackEvent event) {
         double enhancement;
         if (event.getEntity() instanceof ServerPlayer player) {
             enhancement = PlayerExpertisesData.get(player.getLevel()).getExpertises(player).getAll(TrinketerExpertise.class, Skill::isUnlocked).stream().mapToDouble(TrinketerExpertise::getDamageAvoidanceChance).sum();
@@ -26,6 +26,6 @@ public class MixinDiceTrinket {
             enhancement = tree.getAll(TrinketerExpertise.class, Skill::isUnlocked).stream().mapToDouble(TrinketerExpertise::getDamageAvoidanceChance).sum();
         }
 
-        return (float) enhancement;
+        return (float) (instance.getChance() + (.5 * enhancement));
     }
 }
