9ae28f40023a169565a55860bd8a7d575b917fa9
attack8 <attackeight@gmail.com>
2026-04-14 18:10:43 -0400

    Add Mana Reduction Calculation to Ability Descriptions

---
+++ src/main/java/xyz/iwolfking/woldsvaults/mixin/the_vault/skills/MixinAbilityDescriptionFactory.java
@@ -0,0 +1,35 @@
+package xyz.iwolfking.woldsvaults.mixin.the_vault.skills;
+
+import iskallia.vault.gear.attribute.type.VaultGearAttributeTypeMerger;
+import iskallia.vault.init.ModConfigs;
+import iskallia.vault.skill.ability.component.AbilityDescriptionFactory;
+import iskallia.vault.skill.ability.component.AbilityLabelBindingRegistry;
+import iskallia.vault.skill.ability.component.AbilityLabelContext;
+import iskallia.vault.skill.ability.component.AbilityLabelFactory;
+import iskallia.vault.snapshot.AttributeSnapshot;
+import iskallia.vault.snapshot.AttributeSnapshotHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import xyz.iwolfking.woldsvaults.init.ModGearAttributes;
+
+@Mixin(value = AbilityDescriptionFactory.class, remap = false)
+public class MixinAbilityDescriptionFactory {
+
+    @Redirect(method = "appendLabels", at = @At(value = "INVOKE", target = "Liskallia/vault/skill/ability/component/AbilityLabelFactory;create(Ljava/lang/String;Liskallia/vault/skill/ability/component/AbilityLabelContext;)Lnet/minecraft/network/chat/MutableComponent;"))
+    private static MutableComponent modifyManaLabels(String key, AbilityLabelContext<?> context) {
+        if (!key.equals("manaCost")) {
+            return AbilityLabelFactory.create(key, context);
+        } else {
+            AttributeSnapshot snapshot = AttributeSnapshotHelper.getInstance().getSnapshot(Minecraft.getInstance().player);
+            float reduction = snapshot.getAttributeValue(ModGearAttributes.MANA_COST_REDUCTION, VaultGearAttributeTypeMerger.floatSum());
+            return AbilityLabelFactory.create(key, context)
+                    .append(" (" + Integer.parseInt(AbilityLabelBindingRegistry.getBindingValue(context.config(), key)) * (1 - reduction) + ")")
+                    .withStyle(Style.EMPTY.withColor(ModConfigs.COLORS.getColor("manaCost")));
+        }
+    }
+
+}
