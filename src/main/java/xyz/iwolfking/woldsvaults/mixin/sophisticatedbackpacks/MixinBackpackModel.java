411b6d0e11563429ac7643050d991c6eacabe73e
Ruben <unborn998@gmail.com>
2026-04-25 00:48:24 +0200

    WOLDS

--- src/main/java/xyz/iwolfking/woldsvaults/mixin/sophisticatedbackpacks/MixinBackpackModel.java
+++ src/main/java/xyz/iwolfking/woldsvaults/mixin/sophisticatedbackpacks/MixinBackpackModel.java
@@ -19,10 +19,11 @@ public class MixinBackpackModel {
     private static Map<Integer, Item> getBackpackItems() {
         return new LinkedHashMap<>(Map.of(
             0, ModItems.BACKPACK.get(),
-            1, ModItems.IRON_BACKPACK.get(),
-            2, ModItems.GOLD_BACKPACK.get(),
-            3, ModItems.DIAMOND_BACKPACK.get(),
-            4, ModItems.NETHERITE_BACKPACK.get(),
-            5, xyz.iwolfking.woldsvaults.init.ModItems.XL_BACKPACK));
+            1, ModItems.COPPER_BACKPACK.get(),
+            2, ModItems.IRON_BACKPACK.get(),
+            3, ModItems.GOLD_BACKPACK.get(),
+            4, ModItems.DIAMOND_BACKPACK.get(),
+            5, ModItems.NETHERITE_BACKPACK.get(),
+            6, xyz.iwolfking.woldsvaults.init.ModItems.XL_BACKPACK));
     }
 }
